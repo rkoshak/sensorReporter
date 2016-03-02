febd5ff7d6fb63e50da4e5ffad38db9e91247a0f
Rich <rlkoshak@gmail.com>
2016-03-02 16:47:21 -0700

    Fixed a typo and changed how the ON/OFF is calculated for RSII mode

--- bluetoothScanner.py
+++ bluetoothScanner.py
@@ -16,6 +16,7 @@ debug = 0
 
 """Either use 'RSSI' mode, or 'LOOKUP' mode.  RSSI is more reliable."""
 mode = "RSSI"
+#mode = "LOOKUP"
 
 class btSensor:
     """Represents a Bluetooth device"""
@@ -25,6 +26,7 @@ class btSensor:
 
         self.logger = logger
         self.logger.info("----------Configuring BluetoothSensor: Address = " + address + " Destination = " + destination)
+        self.logger.info("---Running in " + mode + " mode")
         self.address = address
         self.state = "OFF"
         self.destination = destination
@@ -32,7 +34,7 @@ class btSensor:
         self.poll = poll
 
         # assume phone is initially far away
-        self.far = True
+        #self.far = True
         self.far_count = 0
         self.near_count = 0
         self.rssi = None
@@ -40,7 +42,7 @@ class btSensor:
         self.publishState()
 
     def getPresence(self):
-        """Detects whether the device is near by or not"""
+        """Detects whether the device is near by or not using lookup_name"""
         result = bluetooth.lookup_name(self.address, timeout=25)
         if(result != None):
             return "ON"
@@ -48,7 +50,7 @@ class btSensor:
             return "OFF"
 
     def getRSSI(self):
-        """Detects whether the device is near by or not"""
+        """Detects whether the device is near by or not using RSSI"""
         addr = self.address
 		
         # Open hci socket
@@ -90,21 +92,43 @@ class btSensor:
             value = self.state
             self.rssi = self.getRSSI()
 		
-            if debug:
-                print "Destination = %s, Current RSSI = %s" % (self.destination, self.rssi)
+            #if debug:
+            #if self.rssi == None:
+            #    self.logger.info("Destination = " + self.destination + ", Current RSSI = None")
+            #else:
+            #    self.logger.info("Found! - Destination = " + self.destination + ", Current RSSI = " + str(self.rssi))
 
             if self.rssi == None:
                 self.far_count += 1
-                self.near_count = 0
-                if self.far_count > 10:
-                    value = "OFF"
-            elif self.rssi < 0:
-                self.far_count = 0
+                self.near_count -= 1
+                if self.near_count < 0:
+                    self.near_count = 0
+                if self.far_count > 20:
+                    self.far_count = 20
+                #self.logger.info("Destination " + self.destination + " not found")
+                #if self.far_count > 10:
+                #    value = "OFF"
+            elif self.rssi <= 0:
+                self.far_count -= 1
                 self.near_count += 1
-                if self.near_count > 10:
-                    value = "ON"
+                if self.far_count < 0:
+                    self.far_count = 0
+                if self.near_count > 20:
+                    self.near_count = 20
+                #self.logger.info("Destination " + self.destination + " detected")
+                #if self.near_count > 10:
+                #    value = "ON"
+            if self.near_count > self.far_count and self.near_count > 10:
+                value = "ON"
+            elif self.far_count > self.near_count and self.far_count > 10:
+                value = "OFF"
+            else:
+                value = self.state
+            #self.logger.info("Destination " + self.destination + " far count = " + str(self.far_count) + " near count " + str(self.near_count) + " RSSI = " + str(self.rssi))
+            
         elif mode == "LOOKUP":
             value = self.getPresence()
+
         else:
             msg = "Invalid 'mode' specified in 'bluetoothScanner.py' !"
             print msg
