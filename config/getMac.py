4394549ae22cc8a2f530fb73c95defc823a83589
Rich <rlkoshak@gmail.com>
2016-09-01 16:31:23 -0600

    Added check that any received packet is actually an ARP

--- config/getMac.py
+++ config/getMac.py
@@ -3,6 +3,7 @@
 from scapy.all import *
 
 def arp_display(pkt):
+  if ARP in pkt:
     if pkt[ARP].op == 1: #who-has (request)
         if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
             print "ARP Probe from: " + pkt[ARP].hwsrc
