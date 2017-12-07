b57924b17a3398184fcf8b7c2b33bd5be16e6e05
Mark Radbourne <markrad@msn.com>
2017-12-07 06:35:54 +0000

    Make GPIO low when msg is OFF

--- rpiGPIOActuator.py
+++ rpiGPIOActuator.py
@@ -61,7 +61,7 @@ class rpiGPIOActuator:
             GPIO.output(self.pin, GPIO.HIGH)
             logger.info('Toggling pin %s LOW to HIGH' % (self.pin))
         else:
-            out = GPIO.LOW if msg.payload == "ON" else GPIO.HIGH
+            out = GPIO.LOW if msg.payload == "OFF" else GPIO.HIGH
             GPIO.output(self.pin, out)
             
     def on_direct_message(self, msg):
