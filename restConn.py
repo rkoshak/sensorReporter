6097b801148bb96da557a34d37e66d301db4211c
msw1970 <mark.webster@websternet.org.uk>
2018-05-17 19:15:22 +0100

    Fixed indent issue caused by empty def

--- restConn.py
+++ restConn.py
@@ -35,7 +35,7 @@ class restConnection(object):
         self.logger = logger
         self.url = params("URL")
         
-    def register(unused1, unused2):
+#    def register(unused1, unused2):
         # Do nothing
 
     def publish(self, message, destination):
