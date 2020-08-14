d078d83563679b3322f3f9fc19145252a53d8840
Rich Koshak <rlkoshak@gmail.com>
2020-08-14 16:18:14 -0600

    pylint for BT classes.

--- bt/btscan_sensor.py
+++ bt/btscan_sensor.py
@@ -28,8 +28,23 @@ from core.sensor import Sensor
 from core.utils import get_sequential_params
 
 class SimpleBtSensor(Sensor):
+    """Implements a simple scanner that looks for the name of a BT devices given
+    their MAC addresses.
+    """
 
     def __init__(self, publishers, params):
+        """Parses the parameters and prepares to scan for the configured devices.
+        Params:
+            - Poll: must be greater than 25
+            - AddressX: sequential list of MAC addresses to look for
+            - DestinationX: sequential list of destinations to publish ON/OFF to
+            when the corresponding Address is found or not. There must be the
+            same numer of Address and Destiantion fields.
+        Raises:
+            - NoOptionError: when a required parameter doesn't exist
+            - ValueError: when the list of Addresses and Destinations don't
+            match up or Poll is too small.
+        """
         super().__init__(publishers, params)
 
         addresses = get_sequential_params(params, "Address")
@@ -46,6 +61,9 @@ class SimpleBtSensor(Sensor):
         self.log.info("Configured simple BT sensor")
 
     def check_state(self):
+        """Loops through the devices and tries to see if they are reachable over
+        Bluetooth.
+        """
         for address in self.devices:
             result = bluetooth.lookup_name(address, timeout=25)
             self.log.debug("Scanned for %s, result = %s", address, result)
@@ -55,12 +73,33 @@ class SimpleBtSensor(Sensor):
                 self._send(value, self.devices[address])
 
     def publish_state(self):
+        """Publishes the last set of states for each device."""
         for address in self.devices:
             self._send(self.states[address], self.devices[address])
 
 class BtRssiSensor(Sensor):
+    """Initiates an inquiry and then reads the response packets and pulls the
+    RSSI. If the RSSI is negative it's near count is incremented and far count
+    is decremented. When it is not seen, the near count is decremented and far
+    count is incremented. When the near count is greater than the far count and
+    greater than the "Near" parameter ON is published. When the far count is
+    greater than the near count and greater than the "Far" parameter OFF is
+    published.
+    """
 
     def __init__(self, publishers, params):
+        """Parses the parameters in preparation to scanning.
+        Params:
+            - Poll: must be greater than 10
+            - Address: BT MAC address to look for
+            - Destination: where to publish the results
+            - Max: maximum number to count up to for both the near and far counts
+            - Near: number for the near count above which ON will be published
+            - Far: number for the far count above which OFF will be published
+        Raises:
+            - NoOptionError: when a required parameter doesn't exist
+            - ValueError: when Poll is too small.
+        """
         super().__init__(publishers, params)
 
         self.address = params("Address")
@@ -78,6 +117,7 @@ class BtRssiSensor(Sensor):
             raise ValueError("Poll must be greater than 10 seconds.")
 
     def read_inquiry_mode(self, sock):
+        """Cribbed from the PyBluez inquiry-with-rssi.py example"""
         # Save the current filter
         old_filter = sock.getsockopt(bt.SOL_HCI, bt.HCI_FILTER, 14)
 
@@ -102,6 +142,7 @@ class BtRssiSensor(Sensor):
         return mode
 
     def write_inquiry_mode(self, sock, mode):
+        """Cribbed from the PyBluez inquiry-with-rssi.py example"""
         old_filter = sock.getsockopt(bt.SOL_HCI, bt.HCI_FILTER, 14)
 
         # Setup socket filter to receive only events related to the
@@ -126,6 +167,9 @@ class BtRssiSensor(Sensor):
         return 0 if status else -1
 
     def device_inquiry_with_rssi(self, sock):
+        """Cribbed from the PyBluez inquiry-with-rssi.py example. Returns a list
+        of tuples with BT device addresses and the RSSI value.
+        """
         # save the old filter
         old_filter = sock.getsockopt(bt.SOL_HCI, bt.HCI_FILTER, 14)
 
@@ -159,7 +203,7 @@ class BtRssiSensor(Sensor):
             elif event == bt.EVT_CMD_STATUS:
                 status, ncmd, opcode = struct.unpack("BBH", pkt[3:7])
                 if status:
-                    self.warning("Something went wrong")
+                    self.log.warning("Something went wrong")
                     break
             elif event == bt.EVT_INQUIRY_RESULT:
                 pkt = pkt[3:]
@@ -168,18 +212,23 @@ class BtRssiSensor(Sensor):
                     addr = bt.ba2str(pkt[1+6*1:1+6*i+6])
                     results.append((addr, -1))
             else:
-                self.debug("Unrecognized packet type")
+                self.log.debug("Unrecognized packet type")
 
         sock.setsockopt(bt.SOL_HCI, bt.HCI_FILTER, old_filter)
         return results
 
     def get_rssi(self):
+        """Sets up the BT device and collects all the RSSI values it can for a
+        few seconds. Looks through the results and for the device we care about
+        return the first RSSI value we saw.
+        """
         # Open the HCI socket.
         try:
             sock = bt.hci_open_dev(0)
         except Exception as exc:
-            self.log.error("Error accessing bluetooth device: %s\n", exc, traceback.format_exec())
-            return
+            self.log.error("Error accessing bluetooth device: %s\n%s", exc,
+                           traceback.format_exc())
+            return None
 
         # Get the mode.
         try:
@@ -187,7 +236,7 @@ class BtRssiSensor(Sensor):
         except Exception as exc:
             self.log.error("Error reading inquiry mode: %s", exc)
             sock.close()
-            return
+            return None
 
         # Change the mode.
         if mode != 1:
@@ -196,11 +245,11 @@ class BtRssiSensor(Sensor):
             except Exception as exc:
                 self.log.error("Error writing inquiry mode: %s", exc)
                 sock.close()
-                return
+                return None
             if result:
                 self.log.error("Error while setting inquiry mode")
                 sock.close()
-                return
+                return None
 
         # Collect rssi packets.
         results = self.device_inquiry_with_rssi(sock)
@@ -214,6 +263,11 @@ class BtRssiSensor(Sensor):
         return found[0][1] if found else None
 
     def check_state(self):
+        """Gets the RSSI for the device we care about. If it's present (i.e.
+        RSSI < -1) we increment the near count and decrement the far count,
+        doing opposite when it's not present. When the counts get high enough
+        ON or OFF is published.
+        """
         value = self.state
         rssi = self.get_rssi()
         self.log.debug("Device %s has RSSI of %s", self.address.upper(), rssi)
@@ -244,4 +298,5 @@ class BtRssiSensor(Sensor):
             self.publish_state()
 
     def publish_state(self):
-       self._send(self.state, self.destination)
+        """Publishes the last state."""
+        self._send(self.state, self.destination)
